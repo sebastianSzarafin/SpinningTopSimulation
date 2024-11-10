b36eaddd6c8c54253c06ba15aad4162e2ab7a6ac
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2024-11-10 16:22:19 +0100

    sandbox: add gui

--- src/SymSandbox/GuiLayer.hh
+++ src/SymSandbox/GuiLayer.hh
@@ -1,6 +1,8 @@
 #ifndef SYM_BASE_GUILAYER_HH
 #define SYM_BASE_GUILAYER_HH
 
+#include <cmath>
+
 #include "SymBase.hh"
 
 #include "SimulationContext.hh"
@@ -14,39 +16,147 @@ namespace sym
     GuiLayer()  = default;
     ~GuiLayer() = default;
 
+    void update(float dt) override
+    {
+      m_fps_manager.m_frames_count++;
+      m_fps_manager.m_current_time += dt;
+      if (m_fps_manager.m_current_time >= m_fps_manager.m_display_time)
+      {
+        m_fps_manager.m_current_fps  = 1.f / dt;
+        m_fps_manager.m_current_time = 0;
+      }
+    }
+
     void imgui_update(float dt) override
     {
-      ImGui::Begin("Settings");
-      static bool simulation_paused = false;
-      if (ImGui::Button("Reset"))
+      ImGui::Begin("Simulation info");
       {
-        SimulationContext::reset_cube();
-        Application::get().reset_simulation();
-        simulation_paused = false;
-      }
-      ImGui::SameLine();
-      if (simulation_paused)
-      {
-        if (ImGui::Button("Resume"))
+        bool cube_reset = false, simulation_reset = false;
+
+        ImGui::Text("FPS: %.1f", m_fps_manager.m_current_fps);
+        ImGui::Text("Simulation time: %.1f", Clock::now());
+        ImGui::Spacing();
+        /* -------------------- Time step -------------------- */
         {
-          Application::get().resume_simulation();
+          static float s_dt = SimulationData::s_dt;
+          ImGui::SliderFloat("Time step (dt)", &s_dt, .001f, .2f);
+          if (std::fabs(s_dt - SimulationData::s_dt) > FLT_EPSILON && ImGui::IsItemDeactivatedAfterEdit())
+          {
+            simulation_reset     = true;
+            SimulationData::s_dt = s_dt;
+          }
+          ImGui::Spacing();
+        }
+        /* -------------------- Angular Velocity -------------------- */
+        {
+          static float s_av = SimulationData::s_angular_velocity.y;
+          ImGui::SliderFloat("Angular velocity (W)", &s_av, 0, 100);
+          if (std::fabs(s_av - SimulationData::s_angular_velocity.y) > FLT_EPSILON &&
+              ImGui::IsItemDeactivatedAfterEdit())
+          {
+            cube_reset                         = true;
+            SimulationData::s_angular_velocity = { 0, s_av, 0 };
+          }
+          ImGui::Spacing();
+        }
+        /* -------------------- Side Length -------------------- */
+        {
+          static float s_a = SimulationData::s_side_len;
+          ImGui::SliderFloat("Side Length (a)", &s_a, .001f, 5);
+          if (std::fabs(s_a - SimulationData::s_side_len) > FLT_EPSILON)
+          {
+            cube_reset                 = true;
+            SimulationData::s_side_len = s_a;
+          }
+          ImGui::Spacing();
+        }
+        /* -------------------- Density -------------------- */
+        {
+          static float s_d = SimulationData::s_density;
+          ImGui::SliderFloat("Density (Ro)", &s_d, .001f, 100);
+          if (std::fabs(s_d - SimulationData::s_density) > FLT_EPSILON && ImGui::IsItemDeactivatedAfterEdit())
+          {
+            cube_reset                = true;
+            SimulationData::s_density = s_d;
+          }
+          ImGui::Spacing();
+        }
+        /* -------------------- Deviation -------------------- */
+        {
+          static float s_d = SimulationData::s_deviation;
+          ImGui::SliderFloat("Deviation (alpha)", &s_d, 0, 180);
+          if (std::fabs(s_d - SimulationData::s_deviation) > FLT_EPSILON)
+          {
+            cube_reset                  = true;
+            SimulationData::s_deviation = s_d;
+          }
+          ImGui::Spacing();
+        }
+        /* -------------------- Trajectory points -------------------- */
+        {
+          ImGui::InputInt("Trajectory points (n)", &SimulationData::s_trajectory_points);
+          SimulationData::s_trajectory_points = std::max(SimulationData::s_trajectory_points, 0);
+        }
+        /* -------------------- Gravity -------------------- */
+        {
+          static int gravity    = 1;
+          bool gravity_modified = gravity;
+          ImGui::Text("Gravity:");
+          ImGui::RadioButton("On", &gravity, 1);
+          ImGui::SameLine();
+          ImGui::RadioButton("Off", &gravity, 0);
+          if (gravity != gravity_modified) { SimulationData::s_gravity = { 0, gravity ? -G : 0, 0 }; }
+          ImGui::Spacing();
+        }
+
+        if (cube_reset || simulation_reset)
+        {
+          SimulationContext::reset_cube();
+          if (simulation_reset) { Application::get().reset_simulation(); }
+        }
+
+        static bool simulation_paused = false;
+        if (ImGui::Button("Reset"))
+        {
+          SimulationContext::reset_cube();
+          Application::get().reset_simulation();
+          simulation_paused = false;
+        }
+        ImGui::SameLine();
+        if (simulation_paused)
+        {
+          if (ImGui::Button("Resume"))
+          {
+            Application::get().resume_simulation();
+            simulation_paused = false;
+          }
+        }
+        else if (ImGui::Button("Pause"))
+        {
+          Application::get().pause_simulation();
+          simulation_paused = true;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Start"))
+        {
+          SimulationContext::reset_cube();
+          Application::get().start_simulation();
           simulation_paused = false;
         }
       }
-      else if (ImGui::Button("Pause"))
-      {
-        Application::get().pause_simulation();
-        simulation_paused = true;
-      }
-      ImGui::SameLine();
-      if (ImGui::Button("Start"))
-      {
-        SimulationContext::reset_cube();
-        Application::get().start_simulation();
-        simulation_paused = false;
-      }
       ImGui::End();
+
+      // ImGui::ShowDemoWindow();
     }
+
+   private:
+    struct
+    {
+      int m_frames_count         = 0;
+      float m_current_fps        = 0;
+      float m_current_time       = 0;
+      const float m_display_time = .25f;
+    } m_fps_manager;
   };
 } // namespace sym
 
--- src/SymSandbox/SimulationData.hh
+++ src/SymSandbox/SimulationData.hh
@@ -3,19 +3,20 @@
 
 #include "SymBase.hh"
 
+#define G 9.81f
+
 namespace sym
 {
-  static constexpr float G = 9.81f;
-
-  class SimulationData
+  struct SimulationData
   {
-   public:
     static float s_dt;
     static glm::vec3 s_gravity;
     static glm::vec3 s_angular_velocity;
     static float s_side_len;
     static float s_density;
     static float s_deviation;
+
+    static int s_trajectory_points;
   };
 
   inline float SimulationData::s_dt                   = .005f;
@@ -24,6 +25,7 @@ namespace sym
   inline float SimulationData::s_side_len             = 1.f;
   inline float SimulationData::s_density              = 1.f;
   inline float SimulationData::s_deviation            = 10.f;
+  inline int SimulationData::s_trajectory_points      = 100;
 } // namespace sym
 
 #endif // SIMULATIONAPP_SIMULATIONDATA_HH
